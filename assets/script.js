cf63f806abc13cb812022f3c2a00fcb8f05d4046
vinald <okiror1vinald@gmail.com>
2024-10-02 08:30:35 +0300

    html

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const btnMeme = document.getElementById("btnMeme");
+const btnJoke = document.getElementById("btnJoke");
+const btnQuote = document.getElementById("btnQuote");
+const bntRiddle = document.getElementById("btnRiddle");
+const btnShowRiddleAnswer = document.getElementById("show-riddle-answer");
