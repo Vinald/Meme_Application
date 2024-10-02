a1adbf8cec3fdc1450a8600b5fa1e294804df9df
vinald <okiror1vinald@gmail.com>
2024-10-02 08:41:23 +0300

    content section HTML

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,26 @@ const btnJoke = document.getElementById("btnJoke");
 const btnQuote = document.getElementById("btnQuote");
 const bntRiddle = document.getElementById("btnRiddle");
 const btnShowRiddleAnswer = document.getElementById("show-riddle-answer");
+
+// Function: Clear all
+const clearAll = () => {};
+
+// Function: Show Meme
+const showMeme = () => {};
+
+// Function: Show Joke
+const showJoke = () => {};
+
+// Function: Show Quote
+const showQuote = () => {};
+
+// Function: Show Riddle
+const showRiddle = () => {};
+
+// Function: Show Riddle Answer
+const revealAnswers = () => {};
+
+// Function: get random data
+const getRadomData = () => {
+    return data[type][rn(data[type].length)];
+};
